17fb2e2ad087c68e99925d589764879174ebf8fa
HogPro <109669833+HogPro@users.noreply.github.com>
2024-05-30 21:36:44 +0300

    ...

--- run.py
+++ run.py
@@ -18,18 +18,19 @@
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
 from flask import Flask, request, jsonify
-
+print("Running!")
+pipe = pipeline("text-generation", model="gpt2")
 app = Flask(__name__)
 
 @app.route('/sendMessage', methods=['POST'])
 def post_function():
     data = request.json  # Parse the incoming JSON request
     user_message = data.get('userMessage')  # Extract the user message from the JSON data
-    
+    generated_text = pipe(user_message, max_length=100)
     # Example response
     response = {
         'message': 'Data received',
-        'data': 'userMessage123: ' + user_message  # Just echoing the received message for simplicity
+        'data': 'response: ' + generated_text  # Just echoing the received message for simplicity
     }
     return jsonify(response)
 
