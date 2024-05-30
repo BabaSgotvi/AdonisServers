5273be2273b2ff164b80d02b52ae05f365efd17c
HogPro <109669833+HogPro@users.noreply.github.com>
2024-05-30 21:11:48 +0300

    ...

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,9 @@ WORKDIR /app
 # Copy the current directory contents into the container at /app
 COPY . /app
 
-RUN pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-RUN pip install transformers
-RUN pip install flask
-
-# Install any other needed packages specified in requirements.txt
-# RUN pip install --no-cache-dir -r requirements.txt
+# RUN pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+# RUN pip install transformers
+# RUN pip install flask
 
 # Run the specified Python script
 CMD ["python", "run.py"]
--- run.py
+++ run.py
@@ -17,5 +17,21 @@
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
+from flask import Flask, request, jsonify
 
-print("working!");
+app = Flask(__name__)
+
+@app.route('/sendMessage', methods=['POST'])
+def post_function():
+    data = request.json  # Parse the incoming JSON request
+    user_message = data.get('userMessage')  # Extract the user message from the JSON data
+    
+    # Example response
+    response = {
+        'message': 'Data received',
+        'data': 'userMessage123: ' + user_message  # Just echoing the received message for simplicity
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
